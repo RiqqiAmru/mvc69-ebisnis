bf17b3513ae7ce3fbf07cb14a5ab342e1fa9dd11
RiqqiAmru <riqqia00@gmail.com>
2023-05-11 12:27:11 +0700

    pert7

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,9 +1,10 @@
 <div class="container">
     <div class="jumbotron mt-4">
-        <h1 class="display-4">Selamat Datang di Website Saya</h1>
+        <h3 class="display-6">Selamat Datang di Website Saya</h3>
         <p class="lead">Halo nama saya <?= $data['nama']; ?></p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <p>Dibawah ini adalah website yang saya buat, untuk menampilkan Jadwal Perkuliahan STMIK WP, website dibuat menggunakan plugin jQuery<a href="https://datatables.net/">datatables</a> dan bisa diakses secara langsung <a href="https://riqqiamru.github.io/jadwal/">disini</a></p>
+        <div class="ratio ratio-16x9 card">
+            <iframe src="https://riqqiamru.github.io/jadwal/" title="jadwal_stmik-wp" allowfullscreen></iframe>
+        </div>
     </div>
 </div>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,5 +1,12 @@
+<footer class="footer-fixed mt-auto py-3 bg-light">
+  <div class="container">
+    <span class="text-muted">&copy; 2023 - Mohon mengakses laman ini menggunakan jaringan internet.</span>
+  </div>
+</footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.min.js"></script>
 </body>
+
 </html>
